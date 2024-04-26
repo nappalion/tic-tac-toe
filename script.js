dc16a269b3821ddc1174fa97f1657f06dcfd75a3
nipplelion <piolotorrico@yahoo.com>
2024-04-26 16:07:22 -0700

    feat: implement console version of game

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Generates board, places piece, and prints board
 function GameBoard() {
   const rows = 3;
   const cols = 3;
@@ -18,7 +19,7 @@ function GameBoard() {
       row < board.length &&
       col >= 0 &&
       col < board[0].length &&
-      board[row][col] == 0
+      board[row][col].getValue() == 0
     ) {
       board[row][col].addPiece(player);
       return true;
@@ -31,7 +32,7 @@ function GameBoard() {
     for (let i = 0; i < rows; i++) {
       lineToPrint = "";
       for (let j = 0; j < cols; j++) {
-        lineToPrint += board[i][j];
+        lineToPrint += board[i][j].getValue();
       }
       console.log(lineToPrint);
       console.log("------");
@@ -41,6 +42,7 @@ function GameBoard() {
   return { getBoard, placePiece, printBoard };
 }
 
+// Each cell holds a value: 0-no player; 1/2-player
 function Cell() {
   let value = 0;
 
@@ -52,3 +54,49 @@ function Cell() {
 
   return { addPiece, getValue };
 }
+
+function GameController(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) {
+  const board = GameBoard();
+
+  const players = [
+    {
+      name: playerOneName,
+      piece: 1,
+    },
+    {
+      name: playerTwoName,
+      piece: 2,
+    },
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (row, column) => {
+    console.log(
+      `Placing ${
+        getActivePlayer().name
+      }'s piece into row [${row}] and column [${column}]`
+    );
+    board.placePiece(row, column, activePlayer.piece);
+
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  return { playRound, getActivePlayer };
+}
+
